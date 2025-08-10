ee1d86dbf0afdde8fdbb4f9f71efd47a8c38ad66
Eason292 <lieason292@gmail.com>
2025-08-09 17:25:10 -0700

    keeping things simple

--- src/topozero/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-TopoZero: Minimal MCTS-driven analog topology generator using an Euler-trail view.
-Flat layout: vocab+adjacency, MCTS+state, SPICE I/O, and a tiny runner.
-"""
-
-__all__ = ["vocab_and_adjacency", "euler_mcts", "netlist_io"]
