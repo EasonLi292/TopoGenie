bd8b6e5412fbd026ad4a6aa48e78f0d81842f028
Eason292 <lieason292@gmail.com>
2025-08-09 17:30:35 -0700

    keeping things simple

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-
-# Add the src directory to PYTHONPATH so tests can import topozero package
-SRC_PATH = Path(__file__).resolve().parent.parent / "src"
-if SRC_PATH not in map(Path, sys.path):
-    sys.path.insert(0, str(SRC_PATH))
